a978b36e138186701e62456e5972855af3d53708
prakhar <prakhar.pratyush8010@gmail.com>
2025-03-23 11:40:02 +0530

    updates and optimizations

--- main.c
+++ main.c
@@ -1,8 +1,3 @@
-// Tasks
-// handle self collision
-// handle border collision
-// display scores
-// play restart and exit prompts
 #include <raylib.h>
 #include <stdlib.h>
 #include <time.h>
@@ -15,6 +10,7 @@
 #define GRID_WIDTH (SCREEN_WIDTH / GRID_SIZE)
 #define GRID_HEIGHT (SCREEN_HEIGHT / GRID_SIZE)
 
+typedef enum { PLAYING, GAME_OVER } GameState;
 typedef struct {
   int x;
   int y;
@@ -26,19 +22,31 @@ typedef struct {
   Position direction;
   Position food;
 } Snake;
+
 Snake snake;
+int score = 0;
+GameState gameState;
 
 void InitGame();
 void UpdateGame();
 void DrawGame();
+void CheckCollisions();
 
 int main() {
-  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE);
-  SetTargetFPS(TARGET_FPS);
-  InitGame();
 
+  srand(time(NULL));
+
+  InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE);
+
+  SetTargetFPS(TARGET_FPS);
+
+  InitGame();
   while (!WindowShouldClose()) {
-    UpdateGame();
+    if (gameState == PLAYING) {
+      UpdateGame();
+    } else if (gameState == GAME_OVER && IsKeyPressed(KEY_ENTER)) {
+      InitGame();
+    }
     BeginDrawing();
     DrawGame();
     EndDrawing();
@@ -53,9 +61,12 @@ void InitGame() {
   for (int i = 0; i < snake.length; i++) {
     snake.body[i] = (Position){GRID_WIDTH / 2 - i, GRID_HEIGHT / 2};
   }
-  srand(time(NULL));
   snake.food.x = rand() % GRID_WIDTH;
   snake.food.y = rand() % GRID_HEIGHT;
+
+  gameState = PLAYING;
+
+  score = 0;
 }
 
 // handle snake movement logic
@@ -74,29 +85,62 @@ void UpdateGame() {
     snake.direction = (Position){1, 0};
   }
 
-  for (int i = snake.length - 1; i > 0; i--) {
-    snake.body[i] = snake.body[i - 1];
+  // optimizing snake movement
+  Position prev = snake.body[0];
+  Position temp;
+
+  for (int i = 1; i < snake.length; i++) {
+    temp = snake.body[i];
+    snake.body[i] = prev;
+    prev = temp;
   }
   snake.body[0].x += snake.direction.x;
   snake.body[0].y += snake.direction.y;
 
+  // checking for collision with food and updating the score
   if (snake.body[0].x == snake.food.x && snake.body[0].y == snake.food.y) {
     snake.length++;
+    score += 5;
     snake.food.x = rand() % GRID_WIDTH;
     snake.food.y = rand() % GRID_HEIGHT;
   }
-  if (snake.body[0].x < 0 || snake.body[0].x >= GRID_WIDTH ||
-      snake.body[0].y < 0 || snake.body[0].y >= GRID_HEIGHT) {
-    InitGame();
-  }
+
+  // checking for self and border collisions
+  CheckCollisions();
 }
 
 // Draw the snake window and snake and food
 void DrawGame() {
   ClearBackground(BLACK);
+  // displaying game over text and score at game over
+  if (gameState == GAME_OVER) {
+    DrawText("Game Over! Pres Enter to Continue", 0, 0, 30, RAYWHITE);
+    DrawText(TextFormat("Final Score : %d", score), 0, 30, 40, RAYWHITE);
+  }
+
+  // drawing the snake and food
   for (int i = 0; i < snake.length; i++)
     DrawRectangle(snake.body[i].x * GRID_SIZE, snake.body[i].y * GRID_SIZE,
                   GRID_SIZE, GRID_SIZE, GREEN);
   DrawRectangle(snake.food.x * GRID_SIZE, snake.food.y * GRID_SIZE, GRID_SIZE,
                 GRID_SIZE, RED);
+}
+
+void CheckCollisions() {
+  Position head = snake.body[0];
+
+  // border collisions
+  if (head.x < 0 || head.x >= GRID_WIDTH || head.y < 0 ||
+      head.y >= GRID_HEIGHT) {
+    gameState = GAME_OVER;
+    return;
+  }
+
+  // self collisions
+  for (int i = 1; i < snake.length; i++) {
+    if (head.x == snake.body[i].x && head.y == snake.body[i].y) {
+      gameState = GAME_OVER;
+      return;
+    }
+  }
 }
